2326eed2e0aac214301340d9eb39d766c088e040
Ganesh <ganesh@unbxd.com>
2022-05-17 16:33:14 -0400

    feat: minstack with extra space

---
+++ stack/min-stack/prob.py
@@ -0,0 +1,16 @@
+from solution import MinStack
+
+
+# Your MinStack object will be instantiated and called as such:
+obj = MinStack()
+obj.push(18)
+obj.push(19)
+obj.push(29)
+obj.push(15)
+obj.push(16)
+print(obj.top())
+print(obj.getMin())
+obj.pop()
+obj.pop()
+print(obj.top())
+print(obj.getMin())
