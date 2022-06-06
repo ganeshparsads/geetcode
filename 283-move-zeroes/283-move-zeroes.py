8859fa516d4946178f853b950bea4e479d1253ca
ganeshparsads <ganeshprasads2507@gmail.com>
2022-06-06 02:38:25 -0400

    Time: 1255 ms (8.93%), Space: 15.6 MB (16.55%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,18 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count = 0
-        i = 0
-
-        while i < len(nums) - 1:
-            if nums[i] == 0:
+        for i in nums:
+            if i == 0:
                 # keep the i in the same position until u find element apart from 0
-                nums.pop(i)
+                nums.remove(i)
                 nums.append(0)
-                count += 1
-            else:
-                i += 1
-
-            if i + count >= len(nums):
-                break
 
