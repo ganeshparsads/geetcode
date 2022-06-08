8ada471687a53465df6849084eae892bf9edb839
ganeshparsads <ganeshprasads2507@gmail.com>
2022-06-08 19:53:17 -0400

    Time: 69 ms (48.14%), Space: 15.3 MB (23.36%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,4 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # regex
+        # s = re.sub('[^A-Za-z0-9]+', '', s.lower())
+        # return s == s[::-1]
+        
+        # two pointers
         s = re.sub('[^A-Za-z0-9]+', '', s.lower())
-        return s == s[::-1]
+        i = 0
+        j = len(s) - 1
+        
+        for i in range(len(s)//2):
+            if i != j and s[i].lower() != s[j].lower():
+                return False
+            
+            i += 1
+            j -= 1
+
+
+        return True
