6776dbb277413142305d3bf05ee4dd4da685cbc2
Ganesh <ganesh@unbxd.com>
2022-06-08 21:11:39 -0400

    feat: palindrome

---
+++ dynamic-programming/palindrome/solution.py
@@ -0,0 +1,25 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # regex
+        # s = re.sub('[^A-Za-z0-9]+', '', s.lower())
+        # return s == s[::-1]
+        
+        # two pointers
+        s = re.sub('[^A-Za-z0-9]+', '', s.lower())
+        i = 0
+        j = len(s) - 1
+        
+        for i in range(len(s)//2):
+            if i != j and s[i].lower() != s[j].lower():
+                return False
+            
+            i += 1
+            j -= 1
+
+
+        return True
+obj = Solution()
+
+print(obj.isPalindrome("race a car"))
