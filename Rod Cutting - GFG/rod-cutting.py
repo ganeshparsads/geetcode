a65362dd53512cda3e0f995bcd54531e41190a43
ganeshparsads <ganeshprasads2507@gmail.com>
2022-06-08 21:31:03 -0400

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.py
+++ Rod Cutting - GFG/rod-cutting.py
@@ -2,17 +2,17 @@
 
 class Solution:
     def cutRod(self, price, n):
+        dp = [[0 for x in range(n+1)] for x in range(n + 1)]
         arr = [i for i in range(1,n+1)]
-        t = [[0 for i in range(n+1)] for j in range(n+1)]
-        for  i in range(n+1):
-            for j in range(n+1):
-                if i==0 or j==0:
-                    t[i][j] = 0
-                elif arr[i-1] <= j:
-                    t[i][j] = max( (price[i-1]+t[i][j-arr[i-1]]), (t[i-1][j]) )
-                else:
-                    t[i][j] = t[i-1][j]
-        return t[n][n]
+        for i in range(1, n+1):
+          for j in range(1, n+1):
+            if i <= j:
+              dp[i][j] = max(price[i-1] + dp[i][j-i], dp[i-1][j])
+            else:
+              dp[i][j] = dp[i-1][j]
+
+        return dp[n][n]
+
 
 
 #{ 
