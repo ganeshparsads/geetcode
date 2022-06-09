7e9acb612a28085000deaa8165754626f2f27ca6
ganeshparsads <ganeshprasads2507@gmail.com>
2022-06-09 17:08:08 -0400

    Time: 45 ms (47.76%), Space: 14 MB (18.93%) - LeetHub

---
+++ 198-house-robber/198-house-robber.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = []
+
+        for index, i in enumerate(nums):
+            idx = index
+            if idx == 0:
+                dp.append(i)
+            elif idx == 1:
+                dp.append(max(i, dp[0]))
+            else:
+                dp.append(max(i+dp[idx-2], dp[idx-1]))
+
+        return dp[len(nums)-1]
+        
