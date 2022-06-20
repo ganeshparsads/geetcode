fb8556206733132585ae5f1cbdea4bb597aca09c
ganeshparsads <ganeshprasads2507@gmail.com>
2022-06-20 18:42:36 -0400

    Time: 104 ms (5.14%), Space: 16.8 MB (27.54%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -23,7 +23,7 @@ class Solution:
         if self.flag:
             self.inorder(root.right)
     
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    def isValidBSTIteration(self, root: Optional[TreeNode]) -> bool:
         self.inorder(root)
         return self.flag
     
@@ -44,4 +44,22 @@ class Solution:
             root = root.right
         
         return True
+    
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.minMaxHelper(root, -math.inf, math.inf)
         
+    def minMaxHelper(self, root, min, max) -> bool:
+        if not root:
+            return True
+
+        print(root.val, min, max)
+
+        if root.val <= min or root.val >= max:
+            print("Hi")
+            return False
+
+
+        left = self.minMaxHelper(root.left, min, root.val)
+        right = self.minMaxHelper(root.right, root.val, max)
+
+        return left and right
