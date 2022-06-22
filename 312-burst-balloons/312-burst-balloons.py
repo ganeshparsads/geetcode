2784864dcbbf88bd1562b9ffc9171025c98f0e90
ganeshparsads <ganeshprasads2507@gmail.com>
2022-06-21 22:38:27 -0400

    Time: 9468 ms (54.31%), Space: 17.1 MB (62.12%) - LeetHub

---
+++ 312-burst-balloons/312-burst-balloons.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+class Solution:
+    cSum = 0
+
+    def maxCoins(self, nums):
+        dq = deque(nums)
+        dq.append(1)
+        dq.appendleft(1)
+
+        l = len(nums)
+
+        dp = [[-1 for x in range(l + 1)] for x in range(l + 1)]
+
+        return self.chooseCoins(1, len(nums), dq, dp)
+
+    def chooseCoins(self, start, end, nums, dp):
+        if start > end:
+            return 0
+
+        if dp[start][end] != -1:
+            return dp[start][end]
+
+        maxi = 0
+
+        for idx in range(start, end+1):
+            val = nums[start-1]*nums[idx]*nums[end+1]
+            val = self.chooseCoins(start, idx-1, nums, dp) + val + self.chooseCoins(idx+1, end, nums, dp)
+
+            maxi = max(maxi, val)
+
+
+        dp[start][end] = maxi
+
+        return dp[start][end]
