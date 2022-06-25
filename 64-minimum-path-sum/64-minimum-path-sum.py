6897b3a4b13192083e4256eca0217faa54d30238
ganeshparsads <ganeshprasads2507@gmail.com>
2022-06-25 00:39:58 -0400

    Time: 103 ms (92.44%), Space: 15.8 MB (53.72%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,21 +1,20 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        # [[False for x in range(mean + 1)] for x in range(size + 1)]
+        dp = [[0 for x in range(len(grid[0]))] for x in range(len(grid))]
 
-        rows, cols = len(grid), len(grid[0])
-        d = {(rows-1,cols-1):grid[-1][-1]}
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
 
-        def dfs(row, col):
+                # initialization:
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[i][j]
+                elif i == 0:
+                    dp[i][j] = grid[i][j] + dp[i][j-1]
+                elif j == 0:
+                    dp[i][j] = grid[i][j] + dp[i-1][j]
+                # optimal decision
+                else:
+                    dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
 
-            if (row,col) in d: return d[(row, col)]
-
-            if row<rows-1 and col<cols-1:
-                d[(row, col)] =  grid[row][col] + min(dfs(row+1, col), dfs(row, col+1))
-                return d[(row, col)]
-            elif row==rows-1:
-                d[(row, col)] = grid[row][col] + dfs(row, col+1)
-                return d[(row, col)]
-            elif col==cols-1:
-                d[(row, col)] = grid[row][col] + dfs(row+1, col)
-                return d[(row, col)]
-
-        return dfs(0,0)
+        return dp[len(grid)-1][len(grid[0])-1]
