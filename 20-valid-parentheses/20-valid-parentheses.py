e2935fbcb61224ae06ccc5ed799a6872faede8fe
ganeshparsads <ganeshprasads2507@gmail.com>
2022-07-21 21:40:20 -0400

    Time: 72 ms (7.15%), Space: 14 MB (24.32%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -2,20 +2,25 @@ from collections import deque
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = deque()
-        validate_map = {'}': '{', ')': '(', ']': '['}
+#         stack = deque()
+#         validate_map = {'}': '{', ')': '(', ']': '['}
 
-        for i in s:
-            if i not in validate_map:
-                stack.append(i)
-            else:
-                if stack and stack[-1] == validate_map[i]:
-                    stack.pop()
-                else:
-                    return False
-        if stack:
-            return False
+#         for i in s:
+#             if i not in validate_map:
+#                 stack.append(i)
+#             else:
+#                 if stack and stack[-1] == validate_map[i]:
+#                     stack.pop()
+#                 else:
+#                     return False
+#         if stack:
+#             return False
 
-        return True
+#         return True
+
+        # Time Complexity: O(n^2) Space: O(1)
+        while '[]' in s or '()' in s or '{}' in s:
+            s = s.replace('[]','').replace('()','').replace('{}','')
+        return not len(s)
 
             
