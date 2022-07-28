ebd09be9f2c2c32b4cac3814f2577cd653d74b59
ganeshparsads <ganeshprasads2507@gmail.com>
2022-07-28 01:16:55 -0400

    Time: 60 ms (97.19%), Space: 18.6 MB (96.61%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -5,34 +5,61 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+    
+    def __init__(self):
+        self.map = {}
+        self.idx_preorder = 0
+    
+    def buildTree(self, preorder: List[int], inorder: List[int]):
         # null check
-        if not len(preorder):
-            return None
+        # if not len(preorder):
+        #     return None
 
-        # rootNode
-        rootVal = preorder[0]
-        rootNode = TreeNode(rootVal)
+        # # rootNode
+        # rootVal = preorder[0]
+        # rootNode = TreeNode(rootVal)
+
+        # rootIdx = -1
+
+        # # find the rootIndex from the preorder:
+        # rootIdx = inorder.index(rootVal)
+
+        # inLeft = list(inorder[0: rootIdx])
+        # inRight = list(inorder[rootIdx+1: len(inorder)])
+        # preLeft = list(preorder[1: len(inLeft)+1])
+        # preRight = list(preorder[len(inLeft)+1: len(preorder)])
+
+
+
+        # rootNode.left = self.buildTree(preLeft, inLeft)
+        # rootNode.right = self.buildTree(preRight, inRight)
+
+        # return rootNode
+
+        # improved time complexity
+        if not preorder:
+            return None
         
-        rootIdx = -1
+        for idx, i in enumerate(inorder):
+            self.map[i] = idx
         
-        # find the rootIndex from the preorder:
-        rootIdx = inorder.index(rootVal)
+        return self.helper(preorder, 0, len(preorder) - 1)
+    
+    def helper(self, preorder, start, end):
+        if start > end:
+            return None
         
-        inLeft = list(inorder[0: rootIdx])
-        inRight = list(inorder[rootIdx+1: len(inorder)])
-        preLeft = list(preorder[1: len(inLeft)+1])
-        preRight = list(preorder[len(inLeft)+1: len(preorder)])
+        # logic
         
-        rootNode.left = self.buildTree(preLeft, inLeft)
-        rootNode.right = self.buildTree(preRight, inRight)
+        root_val = preorder[self.idx_preorder]
+        self.idx_preorder += 1
+        
+        rootIdx = self.map[root_val]
+        
+        rootNode = TreeNode(root_val)
+        
+        rootNode.left = self.helper(preorder, start, rootIdx - 1)
+        
+        rootNode.right = self.helper(preorder, rootIdx+1, end)
         
         return rootNode
-
-                      
-
-
-        
-        
-        
-        
