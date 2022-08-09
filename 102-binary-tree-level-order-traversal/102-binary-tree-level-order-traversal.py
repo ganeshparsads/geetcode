a4c16cbe65f655107271ffef6e543fde95c6f890
ganeshparsads <ganeshprasads2507@gmail.com>
2022-08-09 02:53:29 -0400

    Time: 79 ms (6.54%), Space: 15 MB (9.89%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -7,27 +7,44 @@
 from collections import deque
 
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        result = []
-        
-        if not root:
-            return result
-        
-        queue = deque()
-        queue.append(root)
-        
-        while queue:
-            size = len(queue)
-            level = []
-            for i in range(size):
-                curr = queue.popleft()
-                level.append(curr.val)
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
-            result.append(level)
+    def __init__(self):
+        self.result = []
 
-        return result
-            
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # bfs
+        # result = []
+
+        # if not root:
+        #     return result
+
+        # queue = deque()
+        # queue.append(root)
+
+        # while queue:
+        #     size = len(queue)
+        #     level = []
+        #     for i in range(size):
+        #         curr = queue.popleft()
+        #         level.append(curr.val)
+        #         if curr.left:
+        #             queue.append(curr.left)
+        #         if curr.right:
+        #             queue.append(curr.right)
+        #     result.append(level)
+        self.dfs(root, 0)
+        return self.result
+
+    def dfs(self, root, level):
+        # base case
+        if not root:
+            return None
+        # logic
+        if len(self.result) == level:
+            self.result.append([])
+        self.result[level].append(root.val)
+        self.dfs(root.left, level+1)
+        self.dfs(root.right, level+1)
+
+        
+        
         
