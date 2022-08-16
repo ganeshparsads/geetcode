cfdd87ebef0f04f2ebdbf786153c06e9cb9573fc
ganeshparsads <ganeshprasads2507@gmail.com>
2022-08-15 22:18:08 -0400

    Time: 487 ms (51.31%), Space: 21.6 MB (34.07%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -15,29 +15,20 @@ class Solution:
         directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
 
         if not len(ones):
-            print(ones)
             return 0
 
         bfsStack = deque()
         curr = ones.pop()
         bfsStack.append(curr)
-        visited = set()
-        
-        count = 1
 
-        # import pdb
-        # pdb.set_trace()
+        count = 1
         
         while bfsStack:
-            size = len(bfsStack)
             curr = bfsStack.pop()
-            visited.add(curr)
             for dirt in directions:
                 nr = curr[0] + dirt[0]
                 nc = curr[1] + dirt[1]
-                
-                print(nr, nc)
-                print(n, m)
+
                 if nr >= 0 and nc >= 0 and nr < n and nc < m and grid[nr][nc] == "1" and (nr, nc) in ones:
                     ones.remove((nr, nc))
                     bfsStack.append((nr, nc))
@@ -45,8 +36,5 @@ class Solution:
             if not bfsStack and ones:
                 bfsStack.append(ones.pop())
                 count += 1
-        
-        # if count != 1:
-        #     count += 1
 
         return count
