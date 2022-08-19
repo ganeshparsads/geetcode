3759e2fb9cfb63d7c2830011b39fa479a9df2ed3
ganeshparsads <ganeshprasads2507@gmail.com>
2022-08-19 00:44:11 -0400

    Time: 56 ms (43.31%), Space: 14.1 MB (35.80%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def __init__(self):
+        self.result = []
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return self.result
+        
+        self.helper(nums, 0, [])
+        self.result.append([])
+        return self.result
+    
+    def helper(self, nums, index, previous):
+        if index >= len(nums):
+            return
+        for i in range(index, len(nums)):
+            parent = previous + [nums[i]]
+            self.result.append(parent)
+            self.helper(nums, i+1, parent)
