f50417ba8fb7bc2e1a023b49aa1db66668957b85
ganeshparsads <ganeshprasads2507@gmail.com>
2022-08-20 00:04:29 -0400

    Time: 33 ms (96.12%), Space: 14.1 MB (82.71%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -3,12 +3,24 @@ class Solution:
         self.result = []
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        if not nums:
-            return self.result
+#         if not nums:
+#             return self.result
 
-        self.helper(nums, 0, [])
-        self.result.append([])
-        return self.result
+#         self.helper(nums, 0, [])
+#         self.result.append([])
+#         return self.result
+
+        # iterative solution
+        final = [[]]
+        for parent in nums:
+            size = len(final)
+            for i in range(size):
+                new_list = list(final[i])
+                new_list.append(parent)
+                final.append(new_list)
+
+        return final
+        
     
     def helper(self, nums, index, previous):
         if index >= len(nums):
@@ -17,3 +29,4 @@ class Solution:
             parent = previous + [nums[i]]
             self.result.append(parent)
             self.helper(nums, i+1, parent)
+
