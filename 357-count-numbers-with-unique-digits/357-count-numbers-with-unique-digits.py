07bf543b44514ee5de95c243e9326a2ba7087f72
ganeshparsads <ganeshprasads2507@gmail.com>
2022-08-24 23:22:44 -0400

    Time: 47 ms (52.01%), Space: 13.9 MB (13.42%) - LeetHub

--- 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.py
+++ 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.py
@@ -1,10 +1,37 @@
+import math
+
 class Solution:
     def __init__(self):
         self.result = 0
+
+    def getNDigitNumbers(self, n):
+        return 10**n - 10**(n-1)
     
     def countNumbersWithUniqueDigits(self, n: int) -> int:
-        self.backtrack(n, "0")
-        return self.result
+        # self.backtrack(n, "0")
+
+        # (duplicate, non-duplicates)
+        # number of numbers which are duplicate 
+        dp = [(0, 0) for i in range(n+1)]
+
+        # initialize DP
+        # number of duplicates in zero is zero
+        # number of duplicates in zero is 1
+        dp[0] = (0, 1)
+
+        for i in range(1, n+1):
+            numberOfdigits = self.getNDigitNumbers(i)
+            print(i)
+            prev_dup, prev_non_dup = dp[i-1]
+            duplicates = prev_dup*10 + (i-1) * prev_non_dup
+            non_duplicates = numberOfdigits - duplicates
+            dp[i] = (duplicates, non_duplicates)
+
+        final_sum = 0
+        for i in range(n+1):
+            final_sum += dp[i][1]
+
+        return final_sum
     
     def backtrack(self, n, path):
         if n == 0:
@@ -15,6 +42,6 @@ class Solution:
 
         for num in str_num:
             # try to get rid of leading zero
-            nlz_path = str(int(path))
-            if num not in nlz_path or nlz_path=="0":
+            new_path = str(int(path))
+            if num not in new_path or new_path=="0":
                 self.backtrack(n-1, nlz_path+num)
