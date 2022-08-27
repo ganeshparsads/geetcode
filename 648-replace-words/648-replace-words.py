131642eb3955026572a3b8ee3f39cfaa4251c48b
ganeshparsads <ganeshprasads2507@gmail.com>
2022-08-26 22:06:14 -0400

    Time: 1918 ms (5.05%), Space: 255.8 MB (5.04%) - LeetHub

---
+++ 648-replace-words/648-replace-words.py
@@ -0,0 +1,103 @@
+from collections import deque
+
+class TrieNode:
+    def __init__(self):
+        self.char = ["" for i in range(26)]
+        self.wordEnd = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+
+    def insert(self, word):
+        curr = self.root
+
+        for i in word:
+            if not curr.char[ord(i) - 97]:
+                curr.char[ord(i) - 97] = TrieNode()
+            curr = curr.char[ord(i) - 97]
+
+        curr.wordEnd = True
+
+    def search(self, word):
+        curr = self.root
+
+        for i in word:
+            if not curr.char[ord(i) - 97]:
+                return False
+            curr = curr.char[ord(i) - 97]
+
+        return curr.wordEnd
+
+
+    def runBfs(self, curr, prefix):
+        result = []
+        bfsStack = deque()
+        bfsStack.append((curr, prefix))
+
+        while bfsStack:
+            size = len(bfsStack)
+
+            for i in range(size):
+                curr, prefix = bfsStack.popleft()
+                if curr.wordEnd:
+                    result.append(prefix)
+                for j in range(26):
+                    if curr.char[j]:
+                        new_prefix = prefix + chr(j+97)
+                        bfsStack.append((curr.char[j], new_prefix))
+
+        return result
+
+    # modify prefix
+    def startsWith(self, word):
+        curr = self.root
+
+        for i in word:
+            if not curr.char[ord(i) - 97]:
+                return {}
+            curr = curr.char[ord(i) - 97]
+
+        group = self.runBfs(curr, word)
+
+        result = {}
+
+        for compl in group:
+            result[compl] = word
+
+        print(result)
+
+        return result
+
+class Solution:
+
+    def __init__(self):
+        pass
+
+    def replaceWords(self, dictionary, sentence):
+        obj = Trie()
+
+        for word in sentence.split(' '):
+            obj.insert(word)
+
+        result = {}
+
+        for word in dictionary:
+            group = obj.startsWith(word)
+            for key, val in group.items():
+                if key not in result:
+                    result[key] = val
+                elif len(val) < len(result[key]):
+                    result[key] = val
+                    
+
+        final_sentence = []
+        for word in sentence.split(' '):
+            if word in result:
+                final_sentence.append(result[word])
+            else:
+                final_sentence.append(word)
+
+        return ' '.join(final_sentence)
