9a3d3506e7ea679202210db0e7b7ccddd3fc22e8
ganeshparsads <ganeshprasads2507@gmail.com>
2022-08-29 12:05:53 -0400

    Time: 384 ms (78.22%), Space: 21.8 MB (31.70%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -2,39 +2,31 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ones = set()
-        
-        n = len(grid)
-        m = len(grid[0])
-        
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == "1":
-                    ones.add((i, j))
-        
-        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-
-        if not len(ones):
+        def search_island(i,j):
+            q=collections.deque()
+            q.append((i,j))
+            direction=[(0,1),(0,-1),(1,0),(-1,0)]
+            
+            while q:
+                temp_r,temp_c=q.popleft()
+                for dr,dc in direction:
+                    row=temp_r+dr
+                    col=temp_c+dc
+                    #r,c=row_size, col_size of grid that is mention below
+                    if -1<row<r and -1<col<c and ((row,col) not in visit) and grid[row][col]=='1':
+                        visit.add((row,col))
+                        q.append((row,col))
+                                 
+        if not grid:
             return 0
-
-        bfsStack = deque()
-        curr = ones.pop()
-        bfsStack.append(curr)
-
-        count = 1
+        r,c=len(grid),len(grid[0])
+        visit=set()
+        island=0
         
-        while bfsStack:
-            curr = bfsStack.pop()
-            for dirt in directions:
-                nr = curr[0] + dirt[0]
-                nc = curr[1] + dirt[1]
-
-                if nr >= 0 and nc >= 0 and nr < n and nc < m and grid[nr][nc] == "1" and (nr, nc) in ones:
-                    ones.remove((nr, nc))
-                    bfsStack.append((nr, nc))
-
-            if not bfsStack and ones:
-                bfsStack.append(ones.pop())
-                count += 1
-
-        return count
+        for i in range(r):
+            for j in range(c):
+                if (i,j) not in visit and grid[i][j]=='1':
+                    visit.add((i,j))
+                    search_island(i,j)
+                    island +=1
+        return island
