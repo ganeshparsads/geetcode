4fcefe9274e29ca290b16ec1e0d1eb418953df66
ganeshparsads <ganeshprasads2507@gmail.com>
2022-08-29 23:33:05 -0400

    Time: 146 ms (37.71%), Space: 14.7 MB (16.27%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -6,15 +6,14 @@ class Solution:
         res = max(nums)
         
         for n in nums:
-            if n == 0:
-                c_min = 1
-                c_max = 1
-            
+            # if n == 0:
+            #     c_min = 1
+            #     c_max = 1
             tmp = c_max*n
             c_max = max(tmp, c_min*n, n)
             c_min = min(tmp, c_min*n, n)
             
-        
+            print(c_max, c_min)
             res = max(res, c_max)
         
         return res
