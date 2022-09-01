49fb1ba1207c2f21b9b7923cb3a5e45e30ef4c34
ganeshparsads <ganeshprasads2507@gmail.com>
2022-09-01 16:26:22 -0400

    Time: 133 ms (74.66%), Space: 19.6 MB (26.29%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,25 +1,41 @@
 import heapq
+from collections import Counter
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = {}
-        for i in nums:
-            if i not in freq:
-                freq[i] = 0
-            freq[i] += 1
+    def topKFrequent(self, nums: List[int], threshold: int) -> List[int]:
+#         freq = {}
+#         for i in nums:
+#             if i not in freq:
+#                 freq[i] = 0
+#             freq[i] += 1
         
-        minHeap = []
+#         minHeap = []
         
-        for key, value in freq.items():
-            if len(minHeap) < k:
-                heapq.heappush(minHeap, (value, key))
-            else:
-                if minHeap[0][0] < value:
-                    heapq.heappop(minHeap)
-                    heapq.heappush(minHeap, (value, key))
+#         for key, value in freq.items():
+#             if len(minHeap) < k:
+#                 heapq.heappush(minHeap, (value, key))
+#             else:
+#                 if minHeap[0][0] < value:
+#                     heapq.heappop(minHeap)
+#                     heapq.heappush(minHeap, (value, key))
         
-        result  = []
-        for each in minHeap:
-            result.append(each[1])
+#         result  = []
+#         for each in minHeap:
+#             result.append(each[1])
         
-        return result
+#         return result
+        # bucket sort way
+        count = Counter(nums)
+        
+        freq = [[] for i in range(len(nums)+1)]
+        
+        for key, val in count.items():
+            freq[val].append(key)
+        
+        result = []
+        for j in range(len(nums), -1, -1):
+            for k in freq[j]:
+                result.append(k)
+                if len(result) == threshold:
+                    print("hi", result)
+                    return result
