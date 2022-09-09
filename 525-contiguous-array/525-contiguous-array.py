60d535d6f577f25e4e3da7bb67c072f083f5439a
ganeshparsads <ganeshprasads2507@gmail.com>
2022-09-09 02:26:47 -0400

    Time: 2504 ms (5.01%), Space: 22.1 MB (5.86%) - LeetHub

---
+++ 525-contiguous-array/525-contiguous-array.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        prefix_sum = [0 for i in range(len(nums))]
+        
+        result = -1
+        
+        for idx, i in enumerate(nums):
+            if idx == 0:
+                if i == 0:
+                    prefix_sum[idx] = -1
+                else:
+                    prefix_sum[idx] = 1
+                continue
+            if i == 1:
+                prefix_sum[idx] = prefix_sum[idx-1] + 1
+            else:
+                prefix_sum[idx] = prefix_sum[idx-1] - 1
+            
+            if prefix_sum[idx] == 0:
+                result = idx + 1
+        
+        print(prefix_sum)
+        
+        hMap = {}
+        
+        for idx, i in enumerate(prefix_sum):
+            if i not in hMap:
+                hMap[i] = (idx+1, idx+1)
+            else:
+                start, end = hMap[i]
+                hMap[i] = (start, idx+1)
+
+        for key, val in hMap.items():
+            start, end = val
+            result = max(result, end - start)
+
+        return result
+        
+        
