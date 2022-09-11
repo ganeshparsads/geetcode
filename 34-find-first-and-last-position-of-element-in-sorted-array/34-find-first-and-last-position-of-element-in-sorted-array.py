bf1ee7fc28a23a2c2c221e453ba99907a6626780
ganeshparsads <ganeshprasads2507@gmail.com>
2022-09-11 14:28:19 -0400

    Time: 183 ms (10.53%), Space: 15.5 MB (47.97%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,23 +1,44 @@
 class Solution(object):
+    def binarySearchMin(self, nums, target):
+        start = 0
+        end = len(nums) - 1
+        minEle = 1000000
+        while(start <= end):
+            mid = start + (end-start)//2
+            
+            if nums[mid] < target:
+                start = mid+1
+            elif nums[mid] > target:
+                end = mid-1
+            elif nums[mid] == target:
+                minEle = min(minEle, mid)
+                end = mid-1
+        
+        if minEle == 1000000:
+            return -1
+        return minEle
+
+    def binarySearchMax(self, nums, target):
+        start = 0
+        end = len(nums) - 1
+        maxEle = -1
+        while(start <= end):
+            mid = start + (end-start)//2
+            
+            if nums[mid] < target:
+                start = mid+1
+            elif nums[mid] > target:
+                end = mid-1
+            elif nums[mid] == target:
+                maxEle = max(maxEle, mid)
+                start = mid+1
+        
+        return maxEle
+    
     def searchRange(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        def search(target):
-            left, right = 0, len(nums)
-            while left < right:
-                mid = left + (right-left)//2
-                if nums[mid] >= target:
-                    right = mid
-                else:
-                    left = mid + 1
-            return left
-
-        first = search(target)
-        last = search(target + 1) - 1
-        if target in nums[first: first+1]:
-            return [first, last]
-        else:
-            return [-1, -1]
+        return [self.binarySearchMin(nums, target), self.binarySearchMax(nums, target)]
