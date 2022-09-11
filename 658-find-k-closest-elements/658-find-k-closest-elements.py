dbba2887e1cea0efc5055f57de35e1a98d9ede9d
ganeshparsads <ganeshprasads2507@gmail.com>
2022-09-11 15:57:34 -0400

    Time: 768 ms (8.76%), Space: 15.5 MB (45.64%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -3,23 +3,40 @@ from heapq import heappush, heappop
 
 class Solution:
     def findClosestElements(self, arr, k, x):
-        diff = list(map(lambda n: (-1*abs(n-x), n), arr))
-        maxHeap = []
+        start = 0
+        end = len(arr) - 1
         
-        for i in diff:
-            # print(i)
-            if len(maxHeap) < k:
-                heappush(maxHeap, i)
+        diff = list(map(lambda n: abs(x-n) , arr))
+        
+        while start <= end:
+            if (end - start)+1 == k:
+                print(start)
+                print(end)
+                return arr[start:end+1]
+            if diff[start] > diff[end]:
+                start += 1
             else:
-                if maxHeap[0][0] < i[0]:
-                    heappop(maxHeap)
-                    heappush(maxHeap, i)
-                elif maxHeap[0][0] == i[0] and maxHeap[0][1] > i[1]:
-                    heappop(maxHeap)
-                    heappush(maxHeap, i[1])
+                end -= 1
+        
+        return []
+            
+#         diff = list(map(lambda n: (-1*abs(n-x), n), arr))
+#         maxHeap = []
+        
+#         for i in diff:
+#             # print(i)
+#             if len(maxHeap) < k:
+#                 heappush(maxHeap, i)
+#             else:
+#                 if maxHeap[0][0] < i[0]:
+#                     heappop(maxHeap)
+#                     heappush(maxHeap, i)
+#                 elif maxHeap[0][0] == i[0] and maxHeap[0][1] > i[1]:
+#                     heappop(maxHeap)
+#                     heappush(maxHeap, i[1])
 
-        result = []
-        for i in maxHeap:
-            result.append(i[1])
-        result = sorted(result)
-        return result
+#         result = []
+#         for i in maxHeap:
+#             result.append(i[1])
+#         result = sorted(result)
+#         return result
