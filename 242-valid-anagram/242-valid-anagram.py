41d508b753d9a7b894c2a8fab195a6356145d1c3
ganeshparsads <ganeshprasads2507@gmail.com>
2022-09-11 16:56:00 -0400

    Time: 46 ms (95.25%), Space: 14.6 MB (35.35%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s1 = Counter(s)
+        s2 = Counter(t)
+        
+        if len(s1) != len(s2):
+            return False
+        
+        for key, val in s1.items():
+            if key in s2 and s1[key] == s2[key]:
+                continue
+            else:
+                return False
+        
+        return True
