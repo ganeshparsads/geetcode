d8d7b6cd769470d3bb0e361ab98ef1122b730ed0
ganeshparsads <ganeshprasads2507@gmail.com>
2022-09-14 17:11:57 -0400

    Time: 4835 ms (5.01%), Space: 27.9 MB (57.45%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -7,43 +7,43 @@ class Solution:
         #     visited.add(i)
         # return -1
         
-        n = len(nums) - 1
-		# gives the maximum binary length of a number in [1,n]
-        max_length = n.bit_length()
-        ans = 0
-        
-        for j in range(max_length):
-			# moves the bits in 1 j positons to the left
-			# thus mask has a 1 in the j-th position and 0s everywhere else
-            mask = 1 << j
-            count = 0
-            for i in range(n + 1):
-				# if nums[i] has a 1 in the j-th position
-                if nums[i] & mask > 0:
-                    count += 1
-				# if i has a 1 in the j-th position
-                if i & mask > 0:
-                    count -= 1
-			#if we found extra 1s in the j-th position add that bit to ans
-            if count > 0:
-                ans |= mask
-                    
-        return ans
 #         n = len(nums) - 1
+# 		# gives the maximum binary length of a number in [1,n]
 #         max_length = n.bit_length()
-        
 #         ans = 0
         
 #         for j in range(max_length):
+# 			# moves the bits in 1 j positons to the left
+# 			# thus mask has a 1 in the j-th position and 0s everywhere else
 #             mask = 1 << j
 #             count = 0
-#             for i in range(n+1):
-#                 if nums[i] & mask:
+#             for i in range(n + 1):
+# 				# if nums[i] has a 1 in the j-th position
+#                 if nums[i] & mask > 0:
 #                     count += 1
-#                 if i & mask:
+# 				# if i has a 1 in the j-th position
+#                 if i & mask > 0:
 #                     count -= 1
-            
-#             if count:
-#                 ans = ans | mask
-        
+# 			#if we found extra 1s in the j-th position add that bit to ans
+#             if count > 0:
+#                 ans |= mask
+                    
 #         return ans
+        n = len(nums) - 1
+        max_length = n.bit_length()
+        
+        ans = 0
+        
+        for j in range(max_length):
+            mask = 1 << j
+            count = 0
+            for i in range(n+1):
+                if nums[i] & mask > 0:
+                    count += 1
+                if i & mask > 0:
+                    count -= 1
+            
+            if count > 0:
+                ans = ans | mask
+        
+        return ans
