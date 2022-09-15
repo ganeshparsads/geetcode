9c6e70272907ae51dbc740a53fd956d9fedd981b
ganeshparsads <ganeshprasads2507@gmail.com>
2022-09-14 22:11:49 -0400

    Time: 79 ms (29.82%), Space: 13.9 MB (25.04%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -20,32 +20,3 @@ class Solution:
             nums[j], nums[findEle] = nums[findEle], nums[j]
             
             nums[findEle+1:] = reversed(nums[findEle+1:])
-        
-        return nums
-        
-        
-        
-        n=len(nums)
-        tmp2=0
-        tmp=-1
-        
-        # find the decreased from behind
-        for i in range(n-1,-1,-1):
-            if nums[i-1]<nums[i]:
-                tmp=i-1
-                break
-        if tmp<0:
-            nums.reverse()
-        else:
-            # find the immediate increase element to swap with
-            for j in range(n-1,-1,-1):
-                if nums[j]>nums[tmp]:
-                    tmp2=j
-                    break
-            
-            # swap the j with i
-            nums[j],nums[tmp]=nums[tmp],nums[j]
-            
-            # reverse the rest of the array from i
-            nums[tmp+1:]=reversed(nums[tmp+1:])
-        return nums
