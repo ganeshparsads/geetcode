3fcc3f888efac9e9747e14634e710f08adc16225
ganeshparsads <ganeshprasads2507@gmail.com>
2022-09-19 00:41:04 -0400

    Time: 105 ms (62.02%), Space: 26.4 MB (19.55%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -8,44 +8,76 @@ from collections import deque
 #         self.right = None
 
 class Solution:
+    def __init__(self):
+        self.pPath = []
+        self.qPath = []
+
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        bfsStack = deque()
+        self.helper(root, p, q, [root])
+        for i in range(min(len(self.pPath), len(self.qPath))):
+            if self.pPath[i].val != self.qPath[i].val:
+                return self.pPath[i-1]
         
-        bfsStack.append((root, [root]))
+        if len(self.pPath) < len(self.qPath):
+            return self.pPath[len(self.pPath) - 1]
+
+        return self.qPath[len(self.qPath) - 1]
         
-        pPath = []
+    
+    def helper(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode', path):
+        # base
+        if not root:
+            return
+        path.append(root)
+        if root.val == p.val:
+            self.pPath = list(path)
+        if root.val == q.val:
+            self.qPath = list(path)
+
+        # logic
+        self.helper(root.left, p, q, path)
+        self.helper(root.right, p, q, path)
+
+        # backtrack
+        path.pop()
+
+#         bfsStack = deque()
         
-        qPath = []
+#         bfsStack.append((root, [root]))
         
-        while bfsStack:
-            size = len(bfsStack)
+#         pPath = []
+        
+#         qPath = []
+        
+#         while bfsStack:
+#             size = len(bfsStack)
             
-            for i in range(size):
-                curr, path = bfsStack.popleft()
+#             for i in range(size):
+#                 curr, path = bfsStack.popleft()
 
-                if curr.val == p.val:
-                    pPath = list(path)
-                
-                if curr.val == q.val:
-                    qPath = list(path)
+#                 if curr.val == p.val:
+#                     pPath = list(path)
 
-                if curr.left:
-                    new_curr = curr.left
-                    new_path = list(path)
-                    new_path.append(curr.left)
-                    bfsStack.append((curr.left, list(new_path)))
+#                 if curr.val == q.val:
+#                     qPath = list(path)
 
-                if curr.right:
-                    new_curr = curr.right
-                    new_path = list(path)
-                    new_path.append(curr.right)
-                    bfsStack.append((curr.right, list(new_path)))
+#                 if curr.left:
+#                     new_curr = curr.left
+#                     new_path = list(path)
+#                     new_path.append(curr.left)
+#                     bfsStack.append((curr.left, list(new_path)))
+
+#                 if curr.right:
+#                     new_curr = curr.right
+#                     new_path = list(path)
+#                     new_path.append(curr.right)
+#                     bfsStack.append((curr.right, list(new_path)))
         
-        for i in range(min(len(pPath), len(qPath))):
-            if pPath[i].val != qPath[i].val:
-                return pPath[i-1]
+#         for i in range(min(len(pPath), len(qPath))):
+#             if pPath[i].val != qPath[i].val:
+#                 return pPath[i-1]
         
-        if len(pPath) < len(qPath):
-            return pPath[len(pPath) - 1]
+#         if len(pPath) < len(qPath):
+#             return pPath[len(pPath) - 1]
 
-        return qPath[len(qPath) - 1]
+#         return qPath[len(qPath) - 1]
