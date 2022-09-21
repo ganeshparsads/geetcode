17bee9d13995ea2bd1fe1ac9027e1cae8c4abfb1
ganeshparsads <ganeshprasads2507@gmail.com>
2022-09-21 04:36:46 -0400

    Time: 51 ms (52.40%), Space: 14 MB (13.02%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -6,8 +6,37 @@
 #         self.right = right
 class Solution:
     
-    # stack implementation
+    # morris implementation
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        curr = root
+
+        while curr != None:
+            # find the last element
+            if curr.left == None:
+                result.append(curr.val)
+                curr = curr.right
+            else:
+                pre = curr.left
+                while pre.right != None and pre.right != curr:
+                    pre = pre.right
+                
+                if pre.right == None:
+                    # create root back when coming from up to down
+                    pre.right = curr
+
+                    # move forward or move down
+                    curr = curr.left
+                else:
+                    # print(curr.val)
+                    result.append(curr.val)
+                    print(pre.val, curr.val, curr.right, result)
+                    pre.right = None
+                    curr = curr.right
+        return result
+    
+    # stack implementation
+    def inorderTraversal2(self, root: Optional[TreeNode]) -> List[int]:
         stack = []
         result = []
         while root or stack:
