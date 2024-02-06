fb652300541d2bf25a76b8c72c355a4f26a5bfa3
ganeshparsads <ganeshprasads2507@gmail.com>
2024-02-06 00:08:50 -0500

    Time: 45 ms (91.49%), Space: 16.7 MB (66.51%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -3,9 +3,23 @@ class Solution:
         self.result = []
     
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        self.helper(candidates, 0, target, [])
+        self.forLoopHelper(candidates, 0, target, [])
         return self.result
-    
+
+    def forLoopHelper(self, candidates, pivot, target, path):
+        # base
+        if target < 0:
+            return
+        if target == 0:
+            self.result.append(path)
+            return
+        
+        # logic
+        for i in range(pivot, len(candidates)):
+            path.append(candidates[i])
+            self.forLoopHelper(candidates, i, target-candidates[i], list(path))
+            path.pop()
+
     def helper(self, candidates, index, target, path):
         # base
         if index == len(candidates) or target < 0:
@@ -15,7 +29,6 @@ class Solution:
             return
         
         # logic
-
         # choose
         path.append(candidates[index])
         self.helper(candidates, index, target - candidates[index], list(path))
