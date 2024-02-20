693da04d508a155e8e75c6815c8dff951c86e1f3
ganeshparsads <ganeshprasads2507@gmail.com>
2024-02-19 21:48:43 -0500

    Time: 37 ms (99.50%), Space: 17.3 MB (86.34%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -3,6 +3,7 @@ class Solution:
         self.result = []
     
     def letterCasePermutation(self, s: str) -> List[str]:
+        self.result.append(s)
         s = list(s)
         self.backtrack(0, s)
 
@@ -10,13 +11,13 @@ class Solution:
 
     def backtrack(self, start, s):
         if start == len(s):
-            self.result.append(''.join(s))
             return
 
         for i in range(start, len(s)):
             if s[i].isalpha():
                 s[i] = chr(ord(s[i]) ^ 32)
+                self.result.append(''.join(s))                
                 self.backtrack(i + 1, s)
                 s[i] = chr(ord(s[i]) ^ 32)
 
-        self.backtrack(len(s), s)
+        # self.backtrack(len(s), s)
