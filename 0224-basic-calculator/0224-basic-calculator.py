ec4da63354df9a7a2dcd5da678900b682dd2806c
ganeshparsads <ganeshprasads2507@gmail.com>
2024-02-25 18:15:21 -0500

    Time: 74 ms (51.76%), Space: 18.3 MB (50.01%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,20 +1,26 @@
 class Solution:
     def calculate(self, s: str) -> int:
         it = 0
-
-        def calc() -> int:
-            nonlocal it
-            def update(op: str, v: int) -> None:
-                if op == "+": stack.append(v)
-                if op == "-": stack.append(-v)
-                if op == "*": stack.append(stack.pop() * v)
-                if op == "/": stack.append(int(stack.pop() / v))
         
-            num, stack, sign = 0, [], "+"
+        def calc():
+            nonlocal it
+            
+            def update(op, v):
+                if op == "+":
+                    stack.append(v)
+                elif op == "-":
+                    stack.append(-v)
+                elif op == "*":
+                    stack.append(stack.pop() * v)
+                else:
+                    stack.append(stack.pop() / v)
+            num = 0
+            stack = []
+            sign = "+"
             
             while it < len(s):
                 if s[it].isdigit():
-                    num = num * 10 + int(s[it])
+                    num = num*10 + int(s[it])
                 elif s[it] in "+-*/":
                     update(sign, num)
                     num, sign = 0, s[it]
@@ -22,9 +28,13 @@ class Solution:
                     it += 1
                     num = calc()
                 elif s[it] == ")":
+                    # last number update
                     update(sign, num)
                     return sum(stack)
+                
                 it += 1
+
+            # last number update                
             update(sign, num)
             return sum(stack)
 
