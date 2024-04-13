450cf4f1a919d2f66bcb5f66aa41bb11698237a9
ganeshparsads <ganeshprasads2507@gmail.com>
2024-04-13 17:50:14 -0400

    Time: 31 ms (81.81%), Space: 16.6 MB (49.67%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,22 +1,25 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        low = 0
-        high = 0
-        
-        for ch in s:
-            if ch != "*":
-                d = 1 if ch == "(" else -1
-                
-                low += d
-                high += d
-            else:
-                low -= 1
-                high += 1
-            
-            if low < 0:
-                low = 0
+        stack = []
+        wildcard_stack = []
 
-            if high < 0:
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            elif char == '*':
+                wildcard_stack.append(i)
+            else:
+                if stack:
+                    stack.pop()
+                elif wildcard_stack:
+                    wildcard_stack.pop()
+                else:
+                    return False
+
+        while stack and wildcard_stack:
+            if stack[-1] > wildcard_stack[-1]:
                 return False
-        
-        return low == 0
+            stack.pop()
+            wildcard_stack.pop()
+
+        return not stack
