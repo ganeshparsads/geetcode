1c209363efcd867723e40cc79e3c4b95e6d25e6a
ganeshparsads <ganeshprasads2507@gmail.com>
2024-06-22 15:49:56 -0700

    Time: 53 ms (70.59%), Space: 24.5 MB (72.72%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,44 +8,41 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head:
-            return None
-        if not head.next:
-            return None
-        
         slow = head
         fast = head
         
+        # middle of linked list
         # find the mid
         while fast.next and fast.next.next:# fast.next for even and fast.next.next for odd
             slow = slow.next
             fast = fast.next.next
         
-        # reverse the 2nd half
-        fast = self.reversed(slow.next)
-        # detach the tree
+        # reverse linked list
+        head2 = self.reverseList(slow.next)
+
         slow.next = None
         
         slow = head
-        # merge
-        while fast:
-            temp = slow.next
-            slow.next = fast
-            fast = fast.next
-            slow.next.next = temp
-            slow = temp
         
+        # connect
+        while head2:
+            temp = head.next
+            head.next = head2
+            head2 = head2.next
+            head.next.next = temp
+            head = temp
+            
+        return head
+        
+            
     
-    def reversed(self, head):
-        prev = None
-        curr = head
-        fast = head.next
+    def reverseList(self, head) -> Optional[ListNode]:
+        res = None
         
-        while fast:
-            curr.next = prev
-            prev = curr
-            curr = fast
-            fast = fast.next
+        while head:
+            nxt = head.next
+            head.next = res
+            res = head
+            head = nxt
         
-        curr.next = prev
-        return curr
+        return res
