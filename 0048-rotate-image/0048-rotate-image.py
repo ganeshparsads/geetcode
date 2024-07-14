97f8162df1443043508a081d9dbbcf742d5b46f3
ganeshparsads <ganeshprasads2507@gmail.com>
2024-07-14 15:48:53 -0700

    Time: 37 ms (67.66%), Space: 16.5 MB (80.54%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,21 +3,12 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        r = len(matrix)
-        c = len(matrix[0])
+        matrix.reverse()
+
+        n = len(matrix)
+        cnt = 0
         
-        for j in range(c):
-            row = r-1
-            for i in range(r):
-                if i > row:
-                    break
-                matrix[i][j], matrix[row][j] = matrix[row][j], matrix[i][j]
-                row -= 1
-        
-        for i in range(r):
-            for j in range(i, c):
+        for i in range(n):
+            for j in range(i, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        
-        
-        
-                
+            cnt += 1
